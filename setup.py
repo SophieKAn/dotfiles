ac82233430cbe1c793c18e8a5107d0e74a1c5518
Sophie Anderson <sophieanderson80@gmail.com>
2020-10-03 14:52:37 -0700

    Add requirements to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version="0.0.1",
     py_modules=["dotfiles"],
     install_requires=["click"],
+    dev_requires=["black"],
+    python_requires=">=3.8",
     entry_points="""
         [console_scripts]
         dotfiles=dotfiles:main
